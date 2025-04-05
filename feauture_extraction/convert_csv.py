74ee0d1cc97a70c064f6405acf7b39a57fabb8c8
erenncetin <147340241+erenncetin@users.noreply.github.com>
2025-04-05 22:52:48 +0300

    with 15 parameters (HSV_Hue Std,HSV_Hue Mean,Hue Mean,Saturation Mean,Skewness B,Value Mean,Saturation Std,Skewness A,Kurtosis A,Correlation,Kurtosis B,Contrast,Homogeneity,Value Std,Hue Std) gives best cross-validation score 0.69

--- feauture_extraction/convert_csv.py
+++ feauture_extraction/convert_csv.py
@@ -22,7 +22,7 @@ def collect_all(folder_paths, feature_types, type_kurtosis=1, type_skewness=1):
                 datas.append(feature_data)
 
     data = pd.concat(datas, axis=1)
-    data.to_csv(f'../Dataset_Test_Eren/Graphs/Datasets/Dataset_Vector.csv', index=False)
+    data.to_csv(f'../Dataset_Test_Eren/Graphs/Datasets/New_Dataset_Vector.csv', index=False)
 
 '''    hue_data, kurtosis_data, skewness_data = pd.read_csv(f'{folder_paths[0]}/General_HueStd.csv'), [], []
     if type_kurtosis == 1:
@@ -43,14 +43,7 @@ def collect_all(folder_paths, feature_types, type_kurtosis=1, type_skewness=1):
 def collect_degrees(folder_paths, type_kurtosis, type_skewness):
     for index,folder_path in enumerate(folder_paths):
         data = []
-        if index == 0 or index == 3 or index == 4 or index == 5: # hue, hsv_hue, saturation, value
-            for files in os.listdir(folder_path):
-                if files.endswith('.csv') and not files.startswith('General'):
-                    datas = pd.read_csv(f'{folder_path}/{files}')
-                    data.append(datas)
-            combined_data = pd.concat(data, ignore_index=True)
-            combined_data.to_csv(f'{folder_path}/General.csv', index=False)
-        elif index == 1: # kurtosis
+        if index == 1: # kurtosis
             if type_kurtosis == 1:
                 for files in os.listdir(f'{folder_path}/Fisher (Normal)'):
                     if files.endswith('.csv') and not files.startswith('General'):
@@ -77,20 +70,38 @@ def collect_degrees(folder_paths, type_kurtosis, type_skewness):
                         data.append(skewness_data)
             combined_data = pd.concat(data, ignore_index=True)
             combined_data.to_csv(f'{folder_path}/{"Default" if type_kurtosis == 1 else "Article"}/General.csv', index=False)
+        else: # hue, hsv_hue, saturation, value
+            for files in os.listdir(folder_path):
+                if files.endswith('.csv') and not files.startswith('General'):
+                    datas = pd.read_csv(f'{folder_path}/{files}')
+                    data.append(datas)
+            combined_data = pd.concat(data, ignore_index=True)
+            combined_data.to_csv(f'{folder_path}/General.csv', index=False)
 
 
 def convert_to_csv(path, file_name, feature_type, type_number):
-    labels = {'hue': ('Hue Mean', 'Hue Std'), 'kurtosis': ('Kurtosis A', 'Kurtosis B'), 'skewness': ('Skewness A', 'Skewness B'), 'hsv_hue': ('HSV_Hue Mean', 'HSV_Hue Std'), 'saturation': ('Saturation Mean', 'Saturation Std'), 'value': ('Value Mean', 'Value Std')}
+    labels = {'hue': ('Hue Mean', 'Hue Std'), 'kurtosis': ('Kurtosis A', 'Kurtosis B'), 'skewness': ('Skewness A', 'Skewness B'), 'hsv_hue': ('HSV_Hue Mean', 'HSV_Hue Std'),
+              'saturation': ('Saturation Mean', 'Saturation Std'), 'value': ('Value Mean', 'Value Std'),
+              'contrast': 'Contrast', 'correlation': 'Correlation', 'homogeneity': 'Homogeneity'}
     data = []
+    df = None
     with open(path, 'r') as file:
         for line in file:
             parts = line.split(',')
             degree = parts[0].split(':')[1].strip()
-            feature_one = parts[1].split(':')[1].strip()
-            feature_two = parts[2].split(':')[1].strip()
-            data.append([degree, feature_one, feature_two])
+            size = 1 if isinstance(labels[feature_type], str) else len(labels[feature_type])
+            if size == 1:
+                feature_one = parts[1].split(':')[1].strip()
+                data.append([degree, feature_one])
+            elif size == 2:
+                feature_one = parts[1].split(':')[1].strip()
+                feature_two = parts[2].split(':')[1].strip()
+                data.append([degree, feature_one, feature_two])
 
-    df = pd.DataFrame(data, columns=['Degree', labels[feature_type][0], labels[feature_type][1]])
+    if size == 1:
+        df = pd.DataFrame(data, columns=['Degree', labels[feature_type]])
+    elif size == 2:
+        df = pd.DataFrame(data, columns=['Degree', labels[feature_type][0], labels[feature_type][1]])
     if feature_type == 'hue':
         df.to_csv(f'../Dataset_Test_Eren/Graphs/{feature_type.capitalize()}/{file_name.split(".")[0]}.csv', index=False)
     elif feature_type == 'kurtosis':
@@ -109,16 +120,21 @@ def convert_to_csv(path, file_name, feature_type, type_number):
         df.to_csv(f'../Dataset_Test_Eren/Graphs/HSV/{feature_type.capitalize()}/{file_name.split(".")[0]}.csv', index=False)
     elif feature_type == 'value':
         df.to_csv(f'../Dataset_Test_Eren/Graphs/HSV/{feature_type.capitalize()}/{file_name.split(".")[0]}.csv', index=False)
+    elif feature_type  == 'contrast':
+        df.to_csv(f'../Dataset_Test_Eren/Graphs/{feature_type.capitalize()}/{file_name.split(".")[0]}.csv', index=False)
+    elif feature_type == 'correlation':
+        df.to_csv(f'../Dataset_Test_Eren/Graphs/{feature_type.capitalize()}/{file_name.split(".")[0]}.csv', index=False)
+    elif feature_type == 'homogeneity':
+        df.to_csv(f'../Dataset_Test_Eren/Graphs/{feature_type.capitalize()}/{file_name.split(".")[0]}.csv', index=False)
 
 def arrange_folders():
-    folder_paths = ['../Dataset_Test_Eren/Graphs/Hue', '../Dataset_Test_Eren/Graphs/Kurtosis', '../Dataset_Test_Eren/Graphs/Skewness', '../Dataset_Test_Eren/Graphs/HSV/Hue', '../Dataset_Test_Eren/Graphs/HSV/Saturation', '../Dataset_Test_Eren/Graphs/HSV/Value']
-    feature_types = ['hue', 'kurtosis', 'skewness', 'hsv_hue', 'saturation', 'value']
+    folder_paths = ['../Dataset_Test_Eren/Graphs/Hue', '../Dataset_Test_Eren/Graphs/Kurtosis', '../Dataset_Test_Eren/Graphs/Skewness', '../Dataset_Test_Eren/Graphs/HSV/Hue',
+                    '../Dataset_Test_Eren/Graphs/HSV/Saturation', '../Dataset_Test_Eren/Graphs/HSV/Value',
+                    '../Dataset_Test_Eren/Graphs/Contrast', '../Dataset_Test_Eren/Graphs/Homogeneity',
+                    '../Dataset_Test_Eren/Graphs/Correlation']
+    feature_types = ['hue', 'kurtosis', 'skewness', 'hsv_hue', 'saturation', 'value', 'contrast', 'correlation', 'homogeneity']
     for index, folder_path in enumerate(folder_paths):
-        if index == 0 or index == 3 or index == 4 or index == 5:
-            for file_name in os.listdir(folder_path):
-                if file_name.endswith('.txt'):
-                    convert_to_csv(f'{folder_path}/{file_name}', file_name, feature_types[index], 0)
-        elif index == 1:
+        if index == 1:
             inner_folder = ['Fisher (Normal)', 'Article']
             for inner_index, folder in enumerate(inner_folder):
                 for file_name in os.listdir(f'{folder_path}/{folder}'):
@@ -130,11 +146,18 @@ def arrange_folders():
                 for file_name in os.listdir(f'{folder_path}/{folder}'):
                     if file_name.endswith('.txt'):
                         convert_to_csv(f'{folder_path}/{folder}/{file_name}', file_name, feature_types[index], 1 if inner_index == 0 else 2)
+        else:
+            for file_name in os.listdir(folder_path):
+                if file_name.endswith('.txt'):
+                    convert_to_csv(f'{folder_path}/{file_name}', file_name, feature_types[index], 0)
 
 
 if __name__ == '__main__':
-    folders = ['../Dataset_Test_Eren/Graphs/Hue', '../Dataset_Test_Eren/Graphs/Kurtosis', '../Dataset_Test_Eren/Graphs/Skewness', '../Dataset_Test_Eren/Graphs/HSV/Hue', '../Dataset_Test_Eren/Graphs/HSV/Saturation', '../Dataset_Test_Eren/Graphs/HSV/Value']
-    features_types = ['hue', 'kurtosis', 'skewness', 'hsv_hue', 'hsv_saturation', 'hsv_value']
+    folders = ['../Dataset_Test_Eren/Graphs/Hue', '../Dataset_Test_Eren/Graphs/Kurtosis', '../Dataset_Test_Eren/Graphs/Skewness', '../Dataset_Test_Eren/Graphs/HSV/Hue',
+                    '../Dataset_Test_Eren/Graphs/HSV/Saturation', '../Dataset_Test_Eren/Graphs/HSV/Value',
+                    '../Dataset_Test_Eren/Graphs/Contrast', '../Dataset_Test_Eren/Graphs/Homogeneity',
+                    '../Dataset_Test_Eren/Graphs/Correlation']
+    features_types = ['hue', 'kurtosis', 'skewness', 'hsv_hue', 'saturation', 'value', 'contrast', 'correlation', 'homogeneity']
 #    collect_degrees(folders, 2, 2)
-#    collect_all(folders, features_types, 2, 2)
+    collect_all(folders, features_types, 2, 2)
 #    arrange_folders()
--- feauture_extraction/data_analysis.py
+++ feauture_extraction/data_analysis.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GroupKFold
 from sklearn.model_selection import cross_val_score
 
-def normalize_data_set():
+def normalize_data_set(group_size=12):
 
-    data = pd.read_csv('../Dataset_Test_Eren/Graphs/Datasets/Dataset_Vector_11.csv')
+    data = pd.read_csv('../Dataset_Test_Eren/Graphs/Datasets/New_Dataset_Vector_14.csv')
     x = data.iloc[:, 1:] # Features
     y = data.iloc[:, 0] # Skin Burn Degrees
 
@@ -17,28 +17,36 @@ def normalize_data_set():
     normalized_data = pd.DataFrame(x_normalized, columns=x.columns)
     normalized_data.insert(0, 'Degrees', y)
 
+    # Add Group ID
+    group_ids = [i // group_size for i in range(len(data))]
+    normalized_data['group_id'] = group_ids
+
     return normalized_data
 
 def train_model(normalized_data):
 
-    x = normalized_data.iloc[:, 1:] # Features
+    x = normalized_data.iloc[:, 1:-1] # Features
     y = normalized_data.iloc[:, 0] # Skin Burn Degrees
+    groups = normalized_data['group_id']
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
-    knn_model = KNeighborsClassifier(n_neighbors=3, metric='manhattan')
-    knn_model.fit(x_train, y_train)
+    gkf = GroupKFold(n_splits=5)
+    scores = []
 
-    y_predict = knn_model.predict(x_test)
-    accuracy = accuracy_score(y_test, y_predict)
-    confusion_m = confusion_matrix(y_test, y_predict)
-    print(f'KNN Accuracy: {accuracy:.2f}')
-    print(f'Confusion Matrix:\n{confusion_m}')
+    for fold, (train_idx, test_idx) in enumerate(gkf.split(x, y, groups)):
+        x_train, x_test = x.iloc[train_idx], x.iloc[test_idx]
+        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
 
-    # Cross-Validation with 10-folds
-    scores = cross_val_score(knn_model, x, y, cv=10)
-    print(f'Cross-Validation Scores: {scores}')
-    print(f'Mean Cross-Validation Score: {scores.mean():.2f}')
+        model = KNeighborsClassifier(n_neighbors=3, metric='manhattan')
+        model.fit(x_train, y_train)
 
+        y_pred = model.predict(x_test)
+        acc = accuracy_score(y_test, y_pred)
+        scores.append(acc)
+
+        print(f"[Fold {fold+1}] Accuracy: {acc:.2f}")
+        print(f"Confusion Matrix:\n{confusion_matrix(y_test, y_pred)}\n")
+
+    print(f"Mean Accuracy: {sum(scores)/len(scores):.2f}")
 
 if __name__ == '__main__':
-    train_model(normalize_data_set())
+    train_model(normalize_data_set(12))
